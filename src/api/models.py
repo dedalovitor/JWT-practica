17b793502ade810b2f873664a45831f4fd2feca3
dedalovitor <dedalovitor@gmail.com>
2024-05-10 11:27:28 +0000

    changes

--- src/api/models.py
+++ src/api/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    pets = db.relationship("Pet", backref="user")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -19,3 +20,12 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    race = db.Column(db.String(120), unique=True, nullable=False)
+    age = db.Column(db.Integer, unique=False, nullable=False)
+    castrated = db.Column(db.Boolean, unique=False, nullable=False, default=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False,)
